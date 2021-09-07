0090736ec8386054a3d45b14fc8166c0be58715d
papermashea <molloy.shea@gmail.com>
2021-09-06 23:48:16 -0400

    exercise 1 tasks 1-3

--- lab01_intro_to_coding/exercises/ex1_data_structures.js
+++ lab01_intro_to_coding/exercises/ex1_data_structures.js
@@ -30,68 +30,95 @@ var years = [
   1869
 ];
 
+
+
 // Task 1
 // Console log the length of each Array
-console.log("There are " + names.length + " museum names in the names array. They include " + names[0] + ", "  + names[1] + ", "  + names[2] + ", "  + names[3] + ", "  + names[4] + ", and "  + names[5] + ".")
-console.log("There are " + URLs.length + " URLs in the URLs array: " + URLs[0] + ", "  + URLs[1] + ", "  + URLs[2] + ", "  + URLs[3] + ", "  + URLs[4] + ", and "  + URLs[5] + ".")
-console.log("There are " + years.length + " years listed in the years arra, including" + years[0] + ", "  + years[1] + ", "  + years[2] + ", "  + years[3] + ", "  + years[4] + ", and "  + years[5] + ".")
+console.log("---- Exercise 1 Task 1 ----")
+console.log("There are " + names.length + " museum names in the original names array. They include " + names[0] + ", "  + names[1] + ", "  + names[2] + ", "  + names[3] + ", "  + names[4] + ", and "  + names[5] + ".")
+console.log("There are " + URLs.length + " URLs in the original URLs array: " + URLs[0] + ", "  + URLs[1] + ", "  + URLs[2] + ", "  + URLs[3] + ", "  + URLs[4] + ", and "  + URLs[5] + ".")
+console.log("There are " + years.length + " years listed in the original years array, including " + years[0] + ", "  + years[1] + ", "  + years[2] + ", "  + years[3] + ", "  + years[4] + ", and "  + years[5] + ".")
 
 
 
-// // Task 2
-// // add a new item to an array
-// var newName = "The International Center of Photography"
-// var newURL = "icp.org"
-// var newYear = 1974
 
-// names.push(/* COMPLETE ME */);
-// URLs[/* COMPLETE ME */] = newURL;
-// years = years.concat(/* COMPLETE ME */);
+// Task 2
+// add a new item to an array
+var newName = "The Museum of Modern Art";
+var newURL = "https://www.moma.org";
+var newYear = 1929;
 
-// // Task 3
-// // construct an Object out of our three Arrays
-// // the result should look similar to this:
-// var result = {
-//   "Museum Name 1": {
-//     URL: "www.museumwebsite.com",
-//     year: 2019
-//   }
-// }
+names.push(newName);
+URLs[6] = newURL;
+years = years.concat([newYear]);
 
-// var museums = {};
-// for (var i = 0; i < names.length; i++) {
-//   var currentName = names[/* COMPLETE ME */];
-//   /* COMPLETE ME */ = URLs[i];
-//   var currentYear = /* COMPLETE ME */;
 
-//   museums[currentName] = {};
-//   museums[currentName]["URL"] = currentURL;
-//   museums[currentName].year = currentYear;
-// }
+console.log("---- Exercise 1 Task 2 ----")
+console.log("Now there are " + names.length + " names: " + names)
+console.log(URLs.length + " new URLs: " + URLs)
+console.log(years.length + " new years: " + years)
 
-// console.log('museums', museums)
 
-// var museums2 = {};
-// names.forEach(function(/* COMPLETE ME*/) {
-//   museums2[n] = {};
 
-//   var currentURL = URLs[i];
-//   var currentYear = years[i];
 
-//   museums2[n].URL = currentURL;
-//   museums2[n]["year"] = currentYear;
-// });
+// Task 3
+// construct an Object out of our three Arrays
+// the result should look similar to this:
+var result = {
+  "Museum Name 1": {
+    URL: "www.museumwebsite.com",
+    year: 2019
+  }
+}
 
-// console.log('museums2', museums2)
+var museums = {};
+for (var i = 0; i < names.length; i++) {
+  var currentName = names[i];
+  var currentURL = URLs[i];
+  var currentYear = years[i];
 
-// // Task
-// // Write a function to add a new museum object, with properties URL and year, to an existing museums object. Call it on museums2
-// function addAMuseum(museums, newName, newURL, newYear){
-//   /* COMPLETE ME */
+  museums[currentName] = {};
+  museums[currentName]["URL"] = currentURL;
+  museums[currentName].year = currentYear;
+}
 
-//   return museums;
-// }
+console.log("---- Exercise 1 Task 3 ----")
+console.log('museums', museums)
 
-// addAMuseum(/* COMPLETE ME */);
 
-// console.log('museums2', museums2);
+//////// made new arrays for names/urls/years to preserve original list and show the difference 
+
+
+var namesAll = names.concat([newName]);
+var URLsAll = URLs.concat([newURL]);  
+var yearsAll = years.concat([newYear]);
+
+
+var museumsAll = {};
+namesAll.forEach(function(n, i) {
+  museumsAll[n] = {};
+
+  var currentURL = URLsAll[i];
+  var currentYear = yearsAll[i];
+
+  museumsAll[n].URL = currentURL;
+  museumsAll[n]["year"] = currentYear;
+});
+
+console.log('museumsAll', museumsAll)
+
+
+
+
+// Task 4
+// Write a function to add a new museum object, with properties URL and year, to an existing museums object. Call it on museums2
+function addAMuseum(museums, newName, newURL, newYear){
+  /* COMPLETE ME */
+
+  return museums;
+}
+
+addAMuseum(/* COMPLETE ME */);
+
+console.log("---- Exercise 1 Task 4 ----")
+console.log('museums2', museums2);
