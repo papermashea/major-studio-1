5a4e6f049225a00c13a605445c981ba03dd88f08
papermashea <molloy.shea@gmail.com>
2021-09-07 18:20:47 -0400

    exercise 6 taske 5 aws wip

--- lab01_intro_to_coding/exercises/ex6_dom_manipulation.js
+++ lab01_intro_to_coding/exercises/ex6_dom_manipulation.js
@@ -29,22 +29,21 @@ console.log("Removing the div with the class rectangle displays the document wit
 
 // Task 4
 // What does the following code do?
-// const viz = document.body.querySelector(".viz");
+const viz = document.body.querySelector(".viz");
 
-// console.log(viz, viz.children);
+console.log(viz, viz.children);
 
-// const addChildToViz = () => {
-//   const newChild = document.createElement("div");
-//   newChild.className = "rectangle";
-//   newChild.style.height = Math.random() * 100 + "px";
-//   viz.appendChild(newChild);
-// };
+const addChildToViz = () => {
+  const newChild = document.createElement("div");
+  newChild.className = "rectangle";
+  newChild.style.height = Math.random() * 100 + "px";
+  viz.appendChild(newChild);
+};
 
-// viz.addEventListener("click", addChildToViz);
+viz.addEventListener("click", addChildToViz);
 
 console.log("---- Exercise 6 Task 4 ----");
 console.log("This code initially runs all the console.log messages from ex1_data_structures.js. When I updated that reference in the html document to ex6_dom_manipulation.js, it adds a new pink/green rectangle to the document when clicked - see img_ex6_task4.png.");
-
 
 
 
@@ -56,31 +55,7 @@ function drawIrisData() {
     .fetch("./iris_json.json")
     .then(data => data.json())
     .then(data => {
-      console.log(data);
-    
-    const viz = document.body.querySelector(".viz");
-
-    console.log(viz, viz.children);
-    
-    const addChildToViz = () => {
-      const newChild = document.createElement("div");
-      newChild.className = "petal";
-      newChild.style.height = data.petallength * 100 + "px";
-      newChild.style.width = data.petalwidth * 1000 + "px";      
-      
-      var flower = {};
-      for (var i = 0; i < data.length; i++) {
-        var species = data.class[i];
-        var petalheight = data.petalheight[i];
-        var petalwidth = petalwidth[i];
-      }
-      
-      
-      viz.appendChild(newChild);
-    };
-    
-    viz.addEventListener("click", addChildToViz);    
-      
+      // console.log(data);
     });
 }
 
@@ -90,9 +65,23 @@ console.log("---- Exercise 6 Task 5 ----");
 console.log("You can see the console log data by popping a new window into the browser and right clicking to inspect (or right clicking in the initial preview window but it's messier to read) - see img_ex6_task5.png. This is different from previous exercises because we're pulling in data in from an external source.");
 
 
-// // Task
-// // Modify the code above to visualize the Iris dataset in the preview of index.html.
-// // Feel free to add additional CSS properties in index.html, or using JavaScript, as you see fit.
+
+// Task
+// Modify the code above to visualize the Iris dataset in the preview of index.html.
+// Feel free to add additional CSS properties in index.html, or using JavaScript, as you see fit.
 
 
+const viz = document.body.querySelector(".viz");
+
+console.log(viz, viz.children);
+
+const addChildToViz = () => {
+  const newChild = document.createElement("div");
+  newChild.className = "petal";
+  newChild.idName = data.clas;
+  newChild.style.height = Math.random() * 100 + "px";
+  viz.appendChild(newChild);
+};
+
+viz.addEventListener("click", addChildToViz);
 
