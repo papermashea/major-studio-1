db547f1c0142d984177e97df04d8da2a16d67fce
readyletsgo <rich@rt80.net>
2021-09-09 11:17:33 -0400

    clarified naming for js and title pages

--- lab02_smithsonian_api/ex3_api_fetch_search/fetch_search.js
+++ lab02_smithsonian_api/ex3_api_fetch_search/fetch_search.js
@@ -2,7 +2,7 @@
 // check API documentation for search here: http://edan.si.edu/openaccess/apidocs/#api-search-search
 
 // put your API key here;
-const apiKey = "";  
+const apiKey = "qEIgONMDA9l4wPFA864Vvldpm2tfKzD2LJtAELFK";  
 
 // search base URL
 const searchBaseURL = "https://api.si.edu/openaccess/api/v1.0/search";
