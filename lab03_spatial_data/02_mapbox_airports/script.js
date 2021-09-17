ad52c9481075a8aa572493cb20661b41835aa1ff
EC2 Default User <ec2-user@ip-172-31-28-63.ec2.internal>
2021-09-17 01:34:15 +0000

    d3 and mapbox class demos

--- lab03_spatial_data/02_mapbox_airports/script.js
+++ lab03_spatial_data/02_mapbox_airports/script.js
@@ -1,13 +1,13 @@
 //   Register to get your Mapbox access token https://docs.mapbox.com/help/glossary/access-token/
 //   Code from https://docs.mapbox.com/help/tutorials/custom-markers-gl-js/ 
 
-mapboxgl.accessToken = 'pk.yourMapboxAccessToken';
+mapboxgl.accessToken = 'pk.eyJ1IjoicGFwZXJtYXNoZWEiLCJhIjoiY2szbnh6bGI4MXY1cjNjbjFkMnZvcjQ1ayJ9.MKO-bDpbg-5sZ2sIN8MJ5Q';
 
   var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/light-v10',
-    center: [-96, 37.8],
-    zoom: 1
+    style: 'mapbox://styles/papermashea/cktnnz6hp0gr318liv6t0ac2u',
+    center: [-73, 40],
+    zoom: 6
   });
 
   /*** load data ***/
