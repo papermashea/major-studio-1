8f72941427dd83e82a60d34efc4a0157c873891f
papermashea <molloy.shea@gmail.com>
2021-09-30 01:41:44 -0400

    xscale is mapping literally

--- 1.Quantitative/public/main.js
+++ 1.Quantitative/public/main.js
@@ -1,52 +1,24 @@
+//// global
+let dataset = [];
+
 //// chart
-    var margin = {top: 10, right: 20, bottom: 30, left: 20},
-      width = 5000 - margin.left - margin.right,
+    var margin = {top: 200, right: 20, bottom: 30, left: 20},
+      width = 2000 - margin.left - margin.right,
       height = 470 - margin.top - margin.bottom;
 
     var svg = d3.select("#pantone_viz")
     .append("svg")
-      .attr("width", width + margin.left + margin.right)
+      .attr("width", width)
       .attr("height", height + margin.top + margin.bottom)
     .append("g")
       .attr("transform",
             "translate(" + margin.left + "," + margin.top + ")");
-
-    let Count = {
-        year: "year",
-        year_acquired: "year_acquired"
-    };
-
-    // legend
-    let Legend = {
-        year: "Year created",
-        year_acquired: "Year acquired"
-    };
-
-    let chartState = {};
-
-    chartState.measure = Count.id;
-    chartState.legend = Legend.year;
 
     /// palette 
     //// YELLOW = #F5DF4D  | GREY = #939597
     let colors = d3.scaleOrdinal()
         .domain(["F5DF4D", "939597"])
         .range(['#F5DF4D','#939597']);
-
-    d3.select("yellow").style("color", colors("F5DF4D"));
-    d3.select("grey").style("color", colors("939597"));
-
-    let xScale = d3.scaleLinear()
-        .range([margin.left, width - margin.right]);
-
-    //// x axis
-    var x = d3.scaleTime()
-      .domain([new Date("1910"), new Date("2020")])
-      .range([ 0, width ]);
-
-    svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
 
     //// tooltip
     let tooltip = d3.select("#pantone_viz").append("div")
@@ -58,86 +30,70 @@
         .attr("stroke-dasharray", "1,2");
 
 
+// data = {
+//   const text = await FileAttachment("data/2021_pantone_sm.csv").text();
+//   const parseYear = d3.utcParse("%Y-%m-%d");
+//   return d3.csvParse(text, ({title, color, year}) => ({
+//     title: title,
+//     color: color,
+//     year: parseDate(year),
+//     temperature: +temperature
+//   }));
+// }
+
+// data = d3.csvParse(await FileAttachment("data/2021_pantone_sm.csv").csv({typed: true})).map(({title: title, color:color, year: year, }) => ({title, color, year: new Date[+year]}));
+// console.log(data)
+    
 d3.csv("data/2021_pantone_sm.csv").then(function (data) {
 
-    let dataSet = data;
-    console.log(dataSet)
 
-    // Set chart domain max value to the largest year
-    xScale.domain(d3.extent(data, function (d) {
-        return +d.year;
-    }));
+    // let dataSet = data;
+    // console.log(dataSet)
 
-    // redraw();
+    var minYear = d3.min(data, d => d.year); 
+    var maxYear = d3.max(data, d => d.year); 
 
-    // // Listen to click on "year" and "year_acquired" buttons and trigger redraw when they are clicked
-    // d3.selectAll(".measure").on("click", function() {
-    //     let thisClicked = this.value;
-    //     chartState.measure = thisClicked;
-    //     if (thisClicked === Count.year) {
-    //         chartState.legend = Legend.year;
-    //     }
-    //     if (thisClicked === Count.year_acquired) {
-    //         chartState.legend = Legend.year_acquired;
-    //     }
-    //     redraw();
-    // });
+    var x = d3.scaleTime()
+      // .domain(d3.extent(data, d => d.year))
+      // .domain(d3.extent(data, function(d) { return d.year; }))
+      // .domain([new Date("1910"), new Date("2020")])
+      // .domain([d3.min(data, function(d) { return d.year; }), d3.max(data, function(d) { return d.year; })])
+      .domain(minYear, maxYear)
+      .range([margin.left, width - margin.right]);
 
-    // Trigger filter function whenever checkbox is ticked/unticked
-    // d3.selectAll("input").on("change", filter);
+    svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x)
+        .ticks(10)
+        .tickFormat(d3.timeFormat('%Y'))
+        .tickSizeOuter(0));
 
-    // function redraw() {
-
-        //// simulation
-        let simulation = d3.forceSimulation(dataSet)
-            // Apply positioning force to push nodes towards desired position along X axis
-            .force("x", d3.forceX(function(d) {
-                // Mapping of values from year/year_acquired column of dataset to range of SVG chart (<margin.left, margin.right>)
-                return xScale(+d[chartState.measure]);  // This is the desired position
-            }).strength(2))  // Increase velocity
-            .force("y", d3.forceY((height / 2) - margin.bottom / 2))  // // Apply positioning force to push nodes towards center along Y axis
-            .force("collide", d3.forceCollide(4)) // Apply collision force with radius of 9 - keeps nodes centers 9 pixels apart
-            .stop();  // Stop simulation from starting automatically
-
-        for (let i = 0; i < dataSet.length; ++i) {
-            simulation.tick(10);
-        }
-
-        // // Create circles
-        // let dots = svg.selectAll(".dots")
-        //     .data(dataSet, function(d) { return d.color });
-
-        // dots.exit()
-        //     .transition()
-        //     .duration(1000)
-        //     .attr("cx", 0)
-        //     .attr("cy", (height / 2) - margin.bottom / 2)
-        //     .remove();
-
-        // dots.enter()
-        //     .append("circle")
-        //     .attr("class", "dots")
-        //     .attr("cx", 0)
-        //     .attr("cy", (height / 2) - margin.bottom / 2)
-        //     .attr("r", 6)
-        //     .attr("fill", function(d){ return colors(d.color)})
-        //     .merge(dots)
-        //     .transition()
-        //     .duration(1000)
-        //     .attr("cx", function(d) { return d.year; })
-        //     .attr("cy", function(d) { return d.y; });
-
-
-                // Create circles
+    //// dots
         let dots = svg.selectAll(".dots")
-            .data(dataSet, function(d) { return d.color })
+            .data(data, function(d) { return d.color })
             .enter()
             .append("circle")
             .attr("class", "dots")
-            .attr("r", 6)
+            .attr("r", 10)
             .attr("fill", function(d){ return colors(d.color)})
+            // .attr("cx", d => d.x)
             .attr("cx", function(d) { return d.year; })
             .attr("cy", function(d) { return d.y; });
+
+    //// simulation
+            let simulation = d3.forceSimulation(data)
+            // Apply positioning force to push nodes towards desired position along X axis
+            .force("x", d3.forceX(function(d) {
+                // return +d.year);  // This is the desired position
+            }).strength(2))  // Increase velocity
+            .force("y", d3.forceY((height / 2) - margin.bottom / 2))  // // Apply positioning force to push nodes towards center along Y axis
+            .force("collide", d3.forceCollide(9)) // Apply collision force with radius of 9 - keeps nodes centers 9 pixels apart
+            .stop();  // Stop simulation from starting automatically
+
+        // Manually run simulation
+        for (let i = 0; i < data.length; ++i) {
+            simulation.tick(10);
+        }
 
         //// tool tip on hover
         d3.selectAll(".dots").on("mousemove", function(d) {
@@ -160,45 +116,6 @@ d3.csv("data/2021_pantone_sm.csv").then(function (data) {
 
 });
 
-    //// filter data with checkboxes
-//     function filter() {
-
-//         function getCheckedBoxes(checkboxName) {
-
-//             let checkboxes = d3.selectAll(checkboxName).nodes();
-//             let checkboxesChecked = [];
-//             for (let i = 0; i < checkboxes.length; i++) {
-//                 if (checkboxes[i].checked) {
-//                     checkboxesChecked.push(checkboxes[i].defaultValue);
-//                 }
-//             }
-//             return checkboxesChecked.length > 0 ? checkboxesChecked : null;
-//         }
-
-//         let checkedBoxes = getCheckedBoxes(".color");
-
-//         let newData = [];
-
-//         if (checkedBoxes == null) {
-//             dataSet = newData;
-//             redraw();
-//             return;
-//         }
-
-//         for (let i = 0; i < color.length; i++){
-//             let newArray = data.filter(function(d) {
-//                 return d.color === checkedBoxes[i];
-//             });
-//             Array.prototype.push.apply(newData, newArray);
-//         }
-
-//         dataSet = newData;
-//         redraw();
-//     }
-
-// }).catch(function (error) {
-//     if (error) throw error;
-// });
 
 
 
