f6388582dca1c8059d5a244fc1965dd80780bda2
shea <molloy.shea@gmail.com>
2021-10-08 02:24:27 +0000

    cooper node module test

--- cooper_test/script.js
+++ cooper_test/script.js
@@ -1,11 +1,13 @@
+// npm install node-cooperhewitt
+// npm install dotenv
+
+const dotenv = require('dotenv');
+
 // cooper api
 var cooperhewitt = require('node-cooperhewitt')
 
-// read access
-// var api_token = '191e509801ac902f9238d52fb8403c8f';
-
 // write access
-var api_token = 'e58ca8017b3e61fe4a988c62323b8b73';
+var api_token = process.env.CH_KEY;;
 
 var method = 'cooperhewitt.objects.getRandom';
 var args = {'access_token': api_token};
