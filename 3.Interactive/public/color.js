9f085e00b729b12c245590e14b1626003e1f2ae9
papermashea <molloy.shea@gmail.com>
2021-11-10 13:50:38 -0500

    working json hover html

--- 3.Interactive/public/color.js
+++ 3.Interactive/public/color.js
@@ -31,9 +31,10 @@
 
 
 //// chart
-    var margin = {top: 40, right: 20, bottom: 30, left: 60},
-      width = 1400 - margin.left - margin.right,
-      height = 1400 - margin.top - margin.bottom;
+    var margin = {top: 20, right: 20, bottom: 30, left: 60},
+      width = 1200 - margin.left - margin.right,
+      height = 900 - margin.top - margin.bottom;
+      // height = d3.max(Y) + (radius + padding) * 2 - margin.top - margin.bottom;
 
     var svg = d3.select("#chart")
     .append("svg")
@@ -43,14 +44,16 @@
       .attr("transform",
             "translate(" + margin.left + "," + margin.top + ")");
 
-    // tooltip
-    let tooltip = d3.select("#chart").append("div")
-        .attr("class", "tooltip")
-        .style("opacity", 0);
+  //// tooltip
+    // let tooltip = d3.select("#chart").append("div")
+    //     .attr("class", "tooltip")
+    //     .style("opacity", 0);
 
-    let xLine = svg.append("line")
-        .attr("stroke", "rgb(96,125,139)")
-        .attr("stroke-dasharray", "1,2");
+    // let xLine = svg.append("line")
+    //     .attr("stroke", "rgb(96,125,139)")
+    //     .attr("stroke-dasharray", "1,2");
+
+
 
 d3.json("data/colorDataY.json").then(function (data) { 
   // let d = data;
@@ -67,21 +70,26 @@ d3.json("data/colorDataY.json").then(function (data) {
     // Compute which data points are considered defined.
     var radius = 3;
     var padding = .5;
+    // var limit = height - margin.bottom - radius - padding;
+    // console.log(limit)
 
     const X = d3.map(data, d => d.year);  
     const I = d3.range(X.length).filter(data => !isNaN(X[data]));
     const Y = dodge(I.map(data => xScale(X[data])), radius * 2 + padding);
-    console.log(Y)
+    // console.log(Y)
+    console.log(height - d3.min(Y) - margin.bottom - radius - padding)
+    // console.log(d3.max(Y))
+    console.log(d3.min(Y),d3.max(Y))
 
     // Compute the default height;
-    // if (height === undefined) height = d3.max(Y) + (radius + padding) * 2 - margin.top - margin.bottom;
+    // var height = d3.max(Y) + (radius + padding) * 2 - margin.top - margin.bottom;
 
     function dodge(X, radius) {
       const Y = new Float64Array(X.length);
-      const radius2 = radius ** 2;
+      const radius2 = radius ** 1; //**2
       const epsilon = 1e-3;
       let head = null, tail = null;
-    
+
       // Returns true if circle ⟨x,y⟩ intersects with any circle in the queue.
       function intersects(x, y) {
         let a = head;
@@ -121,19 +129,31 @@ d3.json("data/colorDataY.json").then(function (data) {
     }
 
 
-  // Create simulation with specified dataset
-  // let simulation = d3.forceSimulation(data)
-  //     // Apply positioning force to push nodes towards desired position along X axis
-  //     .force("x", d3.forceX(function(d) {
-  //         // Mapping of values from total/perCapita column of dataset to range of SVG chart (<margin.left, margin.right>)
-  //         return xScale(+d[chartState.measure]);  // This is the desired position
-  //     }).strength(2))  // Increase velocity
-  //     .force("y", d3.forceY((height / 2) - margin.bottom / 2))  // // Apply positioning force to push nodes towards center along Y axis
-  //     .force("collide", d3.forceCollide(9)) // Apply collision force with radius of 9 - keeps nodes centers 9 pixels apart
-  //     .stop();  // Stop simulation from starting automatically
+// Three function that change the tooltip when user hover / move / leave a cell
 
+    var mouseover = function(event, d) {
+      Tooltip.style("opacity", 1)
+    }
+    var mousemove = function(event, d) {
+      Tooltip
+        .html("<strong>" + d.title + "</strong> | <i>" + d.year + "</i><p>Type: " + d.type + "<br> Medium: " + d.media + "</p>")
+        .style("background-color", d.color)
+        .style("left", (d3.select(this).attr("cx") + 'px'))
+        .style("top", (d3.select(this).attr("cy") + 'px'))
+    }
+    var mouseleave = function(event, d) {
+      Tooltip.style("opacity", 0)
+    }
+
+    var Tooltip = d3.select("#chart")
+      .append("div")
+      .attr("class", "tooltip")
+      .style("padding", "8px")
+      .style("opacity", .4)
+      .style("color", "white");
 
   svg.append("g")
+
 
   svg.append("g")
       .attr("transform", "translate(0," + height + ")")
@@ -148,37 +168,42 @@ d3.json("data/colorDataY.json").then(function (data) {
             .data(data, function(d) { return d.color })
             .enter()
             .append("circle")
-            .attr("class", "dots")
-            .attr("r", radius)
-            .attr("fill", function(d){ return d.color})
-             .attr("cx", function(d){return xScale(d.year)}) 
-            .attr("cy", function(d, i) { return Y[i]});
+              .attr("class", "dots")
+              .attr("r", radius)
+              .attr("fill", function(d){ return d.color})
+              .attr("cx", function(d){return xScale(d.year)}) 
+              .attr("cy", function(d, i) { return height - Y[i] - margin.top - radius - padding })
+            .on("mouseover", mouseover)
+            .on("mousemove", mousemove)
+            .on("mouseleave", mouseleave);
 
-            console.log(data)
+            // console.log(data)
 
         //// tool tip on hover
-        d3.selectAll(".dots").on("mousemove", function(d) {
-            tooltip
-            // .data(data, function(d) { return d.title })
-            .html(`<strong>Drawing, Designs for Souvenir Fans</strong><br><br>
-                            Medium: transparent watercolor, white gouache<br>
-                            Type: Drawing<br>`)
-                .style("background-color", "black")
-                .style("color", "white")
-                .style('top', d3.select(this).attr("cx") + 'px')
-                .style('left', d3.select(this).attr("cy") + 'px')
-                .style("opacity", 0.9);
+        // d3.selectAll(".dots").on("mousemove", function(d, i) {
+        //     tooltip
+        //     // .data(data, function(d, i) { return d.title })
+        //     // .html("<strong>" + ${d.title} + "</strong>")
+        //     // .html(`<strong>Drawing, Designs for Souvenir Fans</strong><br><br>
+        //     //                 Medium: transparent watercolor, white gouache<br>
+        //     //                 Type: Drawing<br>`)
+        //       .html(d.title)
+        //       .style("background-color", "black")
+        //       .style("color", "white")
+        //       .style('top', d3.select(this).attr("cx") + 'px')
+        //       .style('left', d3.select(this).attr("cy") + 'px')
+        //       .style("opacity", 0.9);
 
-            xLine.attr("x1", d3.select(this).attr("cx"))
-                .attr("y1", d3.select(this).attr("cy"))
-                .attr("y2", (height - margin.bottom))
-                .attr("x2",  d3.select(this).attr("cx"))
-                .attr("opacity", 1);
+        //     xLine.attr("x1", d3.select(this).attr("cx"))
+        //         .attr("y1", d3.select(this).attr("cy"))
+        //         .attr("y2", (height - margin.bottom))
+        //         .attr("x2",  d3.select(this).attr("cx"))
+        //         .attr("opacity", 1);
 
-        }).on("mouseout", function(_) {
-            tooltip.style("opacity", 0);
-            xLine.attr("opacity", 0);
-        });
+        // }).on("mouseout", function(_) {
+        //     tooltip.style("opacity", 0);
+        //     xLine.attr("opacity", 0);
+        // });
 
     });
 
